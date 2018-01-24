1a72ddcbe96e1c6584d1df8d3394539b822a534f
kurumkan <joomrise@gmail.com>
2018-01-24 23:05:23 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+app.get('/', (req, res) => {
+  res.json({ message: 'hi there' });
+});
