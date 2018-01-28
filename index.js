64a885b41946fc130cb429a30666d919d0f29eb8
kurumkan <joomrise@gmail.com>
2018-01-28 03:45:14 +0300

    add logout

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const path = require('path');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 require('./models/User');
 require('./services/passport');
 const keys = require('./config/keys');
 const mongoose = require('mongoose');
 const app = express();
 mongoose.connect(keys.mongoURI);
+
+app.use(cookieSession({
+  maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+  keys: [keys.cookieKey]
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
